8252bcd3631f5eb6d50abb1133018277a540bac3
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2018-11-22 01:22:12 -0200

    :sparkles: create a module to handle async actions

---
+++ src/async.js
@@ -0,0 +1,22 @@
+/**
+ * Sleep a function, executes after a defined time.
+ * @param {number} time Time sleeping before execute.
+ * @returns {Promise<void>}
+ */
+export const sleep = (time) => new Promise((resolve) => setTimeout(resolve, time));
+
+/**
+ * Sequentially executes functions returning promises and resolves into a collection
+ * with all results.
+ * @template T
+ * @param {(index?: number) => Promise<T>} λs A list of functions returning promises.
+ * @returns {Promise<T[]>}
+ */
+export const sequentially = (λs) => {
+  return λs.reduce((promise, λ, index) => {
+    return promise.then((items) => {
+      return Promise.resolve(λ(index))
+        .then((item) => [ ...items, item ])
+    });
+  }, Promise.resolve([]));
+};
