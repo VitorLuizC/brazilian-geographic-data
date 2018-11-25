a40cd64ce73e83cc1be7304be3c43e01561a9c64
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2018-11-25 03:00:38 -0200

    :sparkles: add other map to data functions and generated files

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ import { createDataFromRaw } from './data';
 (async () => {
 
   try {
-    await Promise.all([
-      await createFolder('raw'),
-      await createFolder('data')
-    ]);
+    // await Promise.all([
+    //   await createFolder('raw'),
+    //   await createFolder('data')
+    // ]);
 
-    await generateLevels();
+    // await generateLevels();
 
     await Promise.all([
       createDataFromRaw(2, 'regions', ([ id, name ]) => ({
@@ -35,10 +35,78 @@ import { createDataFromRaw } from './data';
         state
       })),
 
-      createDataFromRaw(7, 'geographical-meso-region', ([ id, name, state ]) => ({
+      createDataFromRaw(8, 'geographical-mesoregions', ([ id, name, state ]) => ({
         id,
         name,
         state
+      })),
+
+      createDataFromRaw(9, 'geographic-microregions', ([ id, name, state ]) => ({
+        id,
+        name,
+        state
+      })),
+
+      createDataFromRaw(10, 'districts', ([ id, name, state, city ]) => ({
+        id,
+        name,
+        city,
+        state,
+      })),
+
+      createDataFromRaw(11, 'subdistricts', ([ id, name, state, city ]) => ({
+        id,
+        name,
+        city,
+        state,
+      })),
+
+      createDataFromRaw(13, 'metropolitan-subdivisions', ([ id, name, state, city ]) => ({
+        id,
+        name,
+        city,
+        state,
+      })),
+
+      createDataFromRaw(14, 'integrated-development-regions', ([ id, name, state, city ]) => ({
+        id,
+        name,
+        city,
+        state,
+      })),
+
+      createDataFromRaw(15, 'urban-agglomerations', ([ id, name, state ]) => ({
+        id,
+        name,
+        state
+      })),
+
+      createDataFromRaw(17, 'subnormal-agglomerations', ([ id, name, state, city ]) => ({
+        id,
+        name,
+        city,
+        state,
+      })),
+
+      createDataFromRaw(18, 'areas-of-weighting', ([ id, name, state, city ]) => ({
+        id,
+        name,
+        city,
+        state,
+      })),
+
+      createDataFromRaw(19, 'recalculated-weighting-areas', ([ id, name, state, city ]) => ({
+        id,
+        name,
+        city,
+        state,
+      })),
+
+      createDataFromRaw(20, 'sample-disclosure-areas-for-subnormal-agglomerates', ([ id, name, state, city ]) => ({
+        id,
+        name,
+        city,
+        state,
       })),
     ]);
   } catch (error) {
